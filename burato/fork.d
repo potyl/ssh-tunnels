c4ddaf33d13b308d1ebb4eab98d89b5ced89714c
emmanuel.rodriguez <emmanuel.rodriguez@88881863-d94a-0410-9388-e1c33e201bdb>
2008-04-16 19:26:53 +0000

    Added a license and documented each module.

--- burato/fork.d
+++ burato/fork.d
@@ -1,13 +1,32 @@
-/**
- * Simple wrapper over the system call "fork". This wrapper expects the child's
- * task to be a funciton or a delegate.
+/*
+ * fork.d
+ * 
+ * Copyright (C) 2008 Emmanuel Rodriguez
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
 module burato.fork;
 
-private import std.c.linux.linux:
-	pid_t,
-	fork
-;
+/**
+ * This module provides a simple way for forking processes and executing
+ * arbitrary code in the new process. The code to be execute is provided as a
+ * delegate or as a function.
+ */
+
+private import std.c.linux.linux: pid_t, fork;
 private import std.c.process: _exit;
 
 private import burato.error: ErrnoException;
