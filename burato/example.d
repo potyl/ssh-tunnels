925c73907590e61652d1e69b7fb2dc63197165f9
emmanuel.rodriguez <emmanuel.rodriguez@88881863-d94a-0410-9388-e1c33e201bdb>
2008-07-13 08:40:27 +0000

    Implemented a basic SimpleXML parser

--- burato/example.d
+++ burato/example.d
@@ -22,110 +22,223 @@ module burato.example;
 
 /**
  * This module provides a simple executable that's used to expirement various
- * APIs.
+ * APIs or code snippets.
  */
 
 private import std.stdio;
 private import std.string;
+private import std.file;
 
+private import glib.KeyFile;
+private import glib.SimpleXML;
+private import glib.Str;
 
-private import glib.Source;
-private import glib.Timeout;
-private import glib.MainLoop;
-
-
-class Example {
-
-	class Arg {
-		Example object;
-		gpointer arg;
-		this (Example object, void *arg) {
-			this.object = object;
-			this.arg = arg;
-		}
-	};
-
-	this () {
-	
-		gboolean function(gpointer data) fp  = function gboolean(gpointer data) { 
-//			that.timeoutCallbackExampleD(data);
-			writefln("function");
-			return true;
-		};
-//		Timeout.addSeconds(1, cast(GSourceFunc) fp, null);
-   
-		
-		string str = "Hello world".dup;
-		Arg arg = new Arg(this, cast(gpointer)str);
-		Timeout.addSeconds(1, cast(GSourceFunc) &callback, cast(gpointer)arg);
-
-	
-/*	
-		auto dg = gboolean delegate (gpointer data) {
-			writefln("delegate");
-			return true;
-		};
-*/
-
-//		Timeout.addSeconds(1, &timeoutCallbackExampleC, null);
-//		Timeout.addSeconds(1, cast(GSourceFunc) &this.timeoutCallbackExampleD, null);
-
-/*		extern (C) gboolean aaa (gpointer data) {
-			writefln(">>>aaa");
-			return true;
-		}
-		Timeout.addSeconds(1, &aaa, null);
-*/
-	}
-
-
-	extern (C) private gboolean timeoutCallbackExampleC (gpointer data) {
-		writefln("timeoutCallbackExample C");
-		return true;
-	}
-
-	private gboolean timeoutCallbackExampleD (gpointer data) {
-		auto str = cast(char *) data;
-		writefln("timeoutCallbackExample D: %s", str);
-		return true;
-	}
-
-	extern (C) static gboolean callback(gpointer data) {
-		Arg arg = cast(Arg) data;
-		return arg.object.timeoutCallbackExampleD(arg.arg);
-	}
-}
-
+//private import std.c.stdlib;
 
 /**
  * Main entry point of the program.
  */
 int main (string [] args) {
-	
 	writefln("Test");
 	
-	
-//	Source timeout = Source.sourceNewSeconds(1);
-//	Timeout.addSeconds(1, cast(GSourceFunc) &timeoutCallbackD, null);
+	if (args.length < 1) {
+		writefln("Usage: xml");
+		return 1;
+	}
 
-//	Timeout.addSeconds(1, &timeoutCallbackC, null);
-	
-	
-	Example example = new Example();
-	
-	// Start a main loop
-	MainLoop loop = new MainLoop(null, true);
-	loop.run();
-	
+	string text = cast(char[]) std.file.read(args[1]);
+
+
+	//string text = "<save><connection target='festival'><tunnel target='irc.perl.org' port='6667'/></connection></save>";
+
+	XMLParser parser = new MyXMLParser();
+	parser.parse(text);
+
+	writefln("Ok");
 	return 0;
 }
 
-extern (C) gboolean timeoutCallbackC (gpointer data) {
-	writefln("Callback C");
-	return true;
+
+/**
+ * Custom XML parser.
+ */
+class MyXMLParser : XMLParser {
+	
+	void onStartElement (string name, string [string] attributes) {
+		writefln("<%s>", name);
+		writefln("\tAttributes: %s", attributes);
+	}
+
+	void onEndElement (string name) {
+		writefln("</%s>", name);
+	}
+
+	void onText (string text) {
+		writefln("\tText: %s", text);
+	}
+
+	void onPassthrough (string text) {
+		writefln("\tPassthrough: %s", text);
+	}
 }
 
-gboolean timeoutCallbackD (gpointer data) {
-	writefln("Callback D");
-	return true;
+
+/**
+ * Basic XML Parser (a pseudo SAX parser) that' based on SimpleXML. This parser
+ * is object oriented and relies on methods instead of functions.
+ *
+ * The parser can be overloaded by defining the following methods:
+ *
+ *  onError - called when an error is encoutered
+ *  onStartElement - called when an element is started
+ *  onEndElement - called when an element is ended
+ *  onText - called when text is being processed
+ *  onPassthrough - called when non text data (PI, comments) is bein processed
+ *  
+ * The method parse(xml) can be used to parse an XML string.
+ *
+ * This class gives a more object oriented interface to SimpleXML, futhermore it
+ * also makes the methods look more 'D' as it uses 'D' types for the method
+ * arguments.
+ *
+ * As a drawback the SimpleXML instance as all callbacks registered, even if the
+ * derived class doesn't overload them all.
+ */
+class XMLParser {
+
+	private SimpleXML simpleXML;
+	
+	
+	/**
+	 * Creates a new instance.
+	 *
+	 * Parsing is done through the method parse(xml).
+	 */
+	this () {
+		static GMarkupParser parser = {
+			error: &callbackError,
+			startElement: &callbackStartElement,
+			endElement: &callbackEndElement,
+			text: &callbackText,
+			passthrough: &callbackPassthrough,
+		};
+
+		this.simpleXML = new SimpleXML(
+			cast(GMarkupParser *) &parser, 
+			cast(GMarkupParseFlags) 0, 
+			cast(gpointer) this, 
+			cast(GDestroyNotify) null
+		);
+	}
+
+	
+	/**
+	 *
+	 */
+	private static void callbackError (GMarkupParseContext *context, GError *error, gpointer userData) {
+		XMLParser that = cast(XMLParser) userData;
+		that.onError();
+	}
+
+	
+	private static void callbackStartElement (
+		GMarkupParseContext *context,
+		gchar *elementName,
+		gchar **attributeNames,
+		gchar **attributeValues,
+		gpointer userData,
+		GError **error
+	) {
+
+		// Transform the attributes into an hash
+		string [string] attributes;
+		while (true) {
+			string name = Str.toString(*attributeNames++);
+			string value = Str.toString(*attributeValues++);
+			if (name is null) {break;}
+	
+			attributes[name] = value;
+		}
+
+		string element = Str.toString(elementName);
+
+		// Invoke the callback
+		XMLParser that = cast(XMLParser) userData;
+		that.onStartElement(element, attributes);
+	}
+
+
+	private static void callbackEndElement (
+		GMarkupParseContext *context,
+		gchar *elementName,
+		gpointer userData,
+		GError **error
+	) {
+		string name = Str.toString(elementName);
+
+		// Invoke the callback
+		XMLParser that = cast(XMLParser) userData;
+		that.onEndElement(name);
+	}
+
+
+	private static void callbackText (
+		GMarkupParseContext *context,
+		gchar *text,
+		gsize textLen,
+		gpointer userData,
+		GError **error
+	) {
+		
+		string buffer = toString(text, textLen);
+		
+		// Invoke the callback
+		XMLParser that = cast(XMLParser) userData;
+		that.onText(buffer);
+	}
+
+
+	private static void callbackPassthrough (
+		GMarkupParseContext *context,
+		gchar *passthroughText,
+		gsize textLen,
+		gpointer userData,
+		GError **error
+	)
+	{
+		string buffer = toString	(passthroughText, textLen);
+		
+		// Invoke the callback
+		XMLParser that = cast(XMLParser) userData;
+		that.onPassthrough(buffer);
+	}
+
+
+	private static string toString (gchar *text, gsize textLen) {
+		
+		// Transform the C string into a D string
+		string buffer;
+		buffer.length = textLen;
+		for (gsize i = 0; i < textLen; ++i) {
+			buffer[i] = text[i];
+		}
+		
+		return buffer;
+	}
+
+
+	void onError () {}
+	void onStartElement (string name, string [string] attributes) {}
+	void onEndElement (string name) {}
+	void onText (string text) {}
+	void onPassthrough (string text) {}
+
+
+	/**
+	 * Parses an XML document loaded into a string.
+	 */
+	void parse (string xml) {
+		GError *error;
+		this.simpleXML.parse(xml , xml.length, &error);
+	}
 }
