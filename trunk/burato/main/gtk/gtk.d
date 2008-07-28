39fb8d2ebd56d62819aad795bda0a17a6080c8ef
emmanuel.rodriguez <emmanuel.rodriguez@88881863-d94a-0410-9388-e1c33e201bdb>
2008-07-28 20:59:51 +0000

    Extracted the ListStore

--- trunk/burato/main/gtk/gtk.d
+++ trunk/burato/main/gtk/gtk.d
@@ -76,12 +76,10 @@ private import gtk.MenuItem;
 private import gtk.AboutDialog;
 private import gtk.Dialog;
 
-private import gtk.ListStore;
 private import gtk.TreeView;
 private import gtk.TreeViewColumn;
 private import gtk.CellRendererText;
 private import gtk.TreeIter;
-private import gtk.TreePath;
 
 private import gdk.Keymap;
 private import gdk.Event;
@@ -102,7 +100,7 @@ private import burato.ssh.manager;
 private import burato.ssh.connection;
 private import burato.ssh.tunnel;
 private import burato.network;
-private import burato.save.tunnels;
+private import burato.main.gtk.store;
 
 
 /**
@@ -137,175 +135,6 @@ private const int [] SIGNALS = [
  * NOTE: This variable will be set at runtime.
  */
 private string RESOURCE_PATH = ".";
-
-
-
-/**
- * Custom ListStore used to wrap the tunnels.
- */
-private class TunnelsListStore : ListStore {
-
-
-	private static const GType [] COLUMNS = [
-		GType.STRING,
-		GType.INT,
-		GType.STRING,
-		GType.POINTER,
-	];
-
-
-	private class Item {
-		SshConnection connection;
-		TreeIter iter;
-		
-		this (SshConnection connection, TreeIter iter) {
-			this.connection = connection;
-			this.iter = iter;
-		}
-	}
-
-	/**
-	 * Make sure that we hold a reference to the tunnels. This is because we will
-	 * pass them to the underlying gtk store which will hold a pointer to the
-	 * tunnels. Thus we need to keep a reference to the tunnles otherwise the
-	 * garbage collector might collect the tunnels thinking there are no
-	 * references to them.
-	 */
-	private Item [pid_t] items;
-
-	private const SshManager manager;
-
-
-	this () {
-		super(COLUMNS);
-		this.manager = new SshManager(SIGNALS);
-		this.manager.addOnCloseCallback(&this.onSshConnectionClose);
-		this.manager.addOnCreateCallback(&this.onSshConnectionCreate);
-	}
-	
-	
-	/**
-	 * Creates an SSH connection and adds it to the store.
-	 */
-	void add (string hop, NetworkAddress [] targetAddresses) {
-		this.manager.createSshConnection(hop, targetAddresses);
-	}
-	
-	
-	/**
-	 * Closes the SSH connection that's backed by the given PID.
-	 */
-	void closeSshConnection (pid_t pid) {
-
-		// Find the connection to discard in the hash
-		Item *pointer = (pid in this.items);
-		if (pointer is null) {
-			return;
-		}
-		Item item = *pointer;
-		this.discardSshConnection(item);
-	}
-	
-	
-	/**
-	 * Closes the SSH connection designated by the given TreeIter from the store.
-	 */
-	void closeSshConnection (TreeIter iter) {
-		
-		// The path of the iter to remove
-		TreePath path = this.getPath(iter);
-		
-		// Find the same path in the tree store
-		foreach (Item item; items) {
-			TreePath itemPath = this.getPath(item.iter);
-			
-			if (path.compare(itemPath) == 0) {
-				// Remove the SSH connection corresponding to the given path
-				this.discardSshConnection(item);
-				return;
-			}
-		}
-	}
-	
-	
-	/**
-	 * Callback called by the SshManager once a connection is closed. This usually
-	 * happens when an SSH process dies (and the GUI wasn't involved). In this
-	 * situation we simply remove the connection from the GUI.
-	 */
-	private void onSshConnectionClose (SshConnection connection) {
-		// Simply call the method that closes the SSH connection. Of course there's
-		// no need to close the connection since it's already closed but at least
-		// the connection will be removed from the GUI.
-		this.closeSshConnection(connection.pid);
-	}
-	
-	
-	/**
-	 * Callback called by the SshManager once a connection is created. This is
-	 * more of a confirmation that the actual SSH connection is made.
-	 */
-	private void onSshConnectionCreate (SshConnection connection) {
-		Item item = new Item(connection, this.createIter());
-
-		int pos = 0;
-		this.setValue(item.iter, pos++, connection.hop);
-		// FIXME For the moment the main GUI can create only one tunnel per SSH
-		//       connection. This is because in the past the application could only
-		//       handle one tunnel per connection. Now the framework allows the
-		//       tunneling of multiple ports through one SSH connection.
-		SshTunnel tunnel = connection.tunnels[0];
-
-		this.setValue(item.iter, pos++, tunnel.target.port);
-		this.setValue(item.iter, pos++, tunnel.target.host);
-
-		{
-			Value value = new Value();
-			value.init(GType.POINTER);
-			value.setPointer(cast(void*) tunnel);
-
-			this.setValue(item.iter, pos++, value);
-		}
-
-		this.items[connection.pid] = item;
-	}
-	
-
-	/**
-	 * This method performs the removal of an SSH connection. At first it starts
-	 * by removing the SSH connection from the SSH manager. This will close the
-	 * SSH process if the process is still running and it will remove the iptables
-	 * rules associated with the tunnels. Once the connection is removed from the
-	 * SSH manager it will be also removed from the store, this will have for
-	 * effect of removing the entry from the GUI.
-	 */
-	private void discardSshConnection (Item item) {
-	
-		pid_t pid = item.connection.pid;
-
-		// Remove the SSH connection from the manager (kill pid, remove iptables)
-		this.manager.removeSshConnection(pid);
-
-		// Remove the entry from our items lookup
-		this.items.remove(pid);
-
-		// Remove the entry from the store (the GUI)
-		this.remove(item.iter);
-	}
-
-	
-	void closeAll () {
-		writefln("Application has %d items", this.items.length);
-		//this.manager.closeSshConnections();
-		
-		foreach (Item item; this.items) {
-			SshConnection connection = item.connection;
-			writefln("quit >> Closing tunnel %s PID %d", connection, connection.pid);
-			this.discardSshConnection(item);
-//			connection.disconnect();
-		}
-	}	
-}
 
 
 class Application {
@@ -360,7 +189,7 @@ class Application {
 		
 		this.statusIcon = new StatusIcon(getResourcePath("ssh-tunnels.png"), true);
 		
-		this.store = new TunnelsListStore();
+		this.store = new TunnelsListStore(SIGNALS);
 		
 		this.finalizeWidgets();
 	}
